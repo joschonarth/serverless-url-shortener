94bc8d5969727247efdcce5b9f92bb779b028093
joschonarth <joschonarth@gmail.com>
2024-11-19 17:21:53 -0300

    feat: first commit

---
+++ src/main/java/com/joschonarth/urlambda/Main.java
@@ -0,0 +1,14 @@
+package com.joschonarth.urlambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        return Map.of();
+    }
+}
